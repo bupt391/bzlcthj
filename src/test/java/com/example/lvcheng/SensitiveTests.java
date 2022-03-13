f1df82dc65f86644d9a873946ef377724bcac045
bupt391 <nuaa36103@163.com>
2022-03-13 20:58:14 +0800

    通过构建前缀树过滤敏感词！

---
+++ src/test/java/com/example/lvcheng/SensitiveTests.java
@@ -0,0 +1,23 @@
+package com.example.lvcheng;
+
+
+import com.example.lvcheng.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = LvchengApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void tsetSensitiveFilter(){
+        String text = "这里可以赌博，可以嫖娼，可以吸毒，可以开票，哈哈哈！";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
